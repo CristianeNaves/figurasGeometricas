1d63bf8e1005b448aa6044e414712fbe8caf69d9
GTaumaturgo <gabrieloliveiraunb@gmail.com>
2017-05-07 18:21:00 -0300

    corrigido bugs mas comprotamento ainda não é o ideal

--- converte.c
+++ converte.c
@@ -5,28 +5,30 @@
 
 
 int main(){
-	FILE* in =  fopen("butao.bmp","r+");
-	FILE* out = fopen("butao.bin","w+");
+	FILE* in =  fopen("butao.bmp","rb");
+	FILE* out = fopen("butao.bin","wb");
 	fseek(out,0,SEEK_SET);
 
-	char aux;
-	char acc;
+	unsigned char aux;
+	unsigned char acc;
 	for (int i = 239; i > -1; --i)
 	{
-		fseek(in,54 + (320*i*3), SEEK_SET);
+		fseek(in,54 + (320*i*3),SEEK_SET);
 		for (int j = 0; j < 320; ++j)
 		{
-			fread(&aux,1,1,in);
-			acc = aux >> 5;
+			acc = 0;	
 			fread(&aux,1,1,in);
 			aux = aux >> 5;
-			aux = aux << 3;
+			acc += aux << 5;
+			fread(&aux,1,1,in);
+			aux = aux >> 5;
+			aux = aux << 2;
 			acc += aux;
 			fread(&aux,1,1,in);
 			aux = aux >> 6;
-			aux = aux << 6;
 			acc += aux;
 			fwrite(&acc,1,1,out);
 		}
 	}
+
 }
